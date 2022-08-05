c77d535d913bdcddce149c3c6168cb924e998e72
Mihir Laud <mihirlaud@gmail.com>
2022-08-04 21:39:50 -0400

    Serve Yew sample app

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#![recursion_limit = "2048"]
 use yew::prelude::*;
 
 enum Msg {
+    AddOne,
 }
 
 struct Model {
+    value: i64,
 }
 
 impl Component for Model {
@@ -13,17 +14,28 @@ impl Component for Model {
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
+            value: 0,
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
         html! {
             <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
             </div>
         }
     }
